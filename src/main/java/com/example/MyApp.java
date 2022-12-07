acce3b8291091dbb1492b003521caaf4faccf366
Hubert Tomana <hubertomana@gmail.com>
2022-12-07 17:20:08 +0100

    Zad.1) To show how both messages work

--- src/main/java/com/example/MyApp.java
+++ src/main/java/com/example/MyApp.java
@@ -8,15 +8,17 @@ public class MyApp {
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         MessageService messageService = applicationContext.getBean("messageService", MessageService.class);
-
-        MessageService secondMessageService = applicationContext.getBean("messageService1", MessageService.class);
+        MessageService secondMessageService = applicationContext.getBean("messageService", MessageService.class);
+        MessageService thirdMessageService = applicationContext.getBean("messageService1", MessageService.class);
 
         System.out.println("First messageService : " + messageService.hashCode());
         System.out.println("Second messageService : " + secondMessageService.hashCode());
+        System.out.println("Third messageService : " + thirdMessageService.hashCode());
 
 
         System.out.println(messageService.getMessage());
         System.out.println(secondMessageService.getMessage());
+        System.out.println(thirdMessageService.getMessage());
 
         applicationContext.close();
     }
